bcffe80e8eba935c9976b95c142d2788e23e6cca
Zidiox <guillaume.guthier@gmail.com>
2024-11-27 16:38:19 +0100

    Ajout de la classe Question

---
+++ Question.js
@@ -0,0 +1,11 @@
+// Classe permettant la création d'une question
+
+var Question = function(titre, type, enonce, options, reponsesCorrectes){
+	this.titre = titre;
+	this.type = type;
+	this.enonce = enonce;
+	this.options = options;
+    this.reponsesCorrectes = reponsesCorrectes;
+}
+
+module.exports = Question;
