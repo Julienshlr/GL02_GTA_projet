caff6fa93f7d847a270a6d71bade7eb77baf6f39
Zidiox <guillaume.guthier@gmail.com>
2024-11-27 17:44:22 +0100

    Ajout d'un id à une question et ajout d'une collection de questions permettant l'ajout d'une question par son id, d'en retirer par son id et d'afficher une collection

--- Question.js
+++ Question.js
@@ -1,11 +1,12 @@
 // Classe permettant la création d'une question
 
-var Question = function(titre, type, enonce, options, reponsesCorrectes){
-	this.titre = titre;
-	this.type = type;
-	this.enonce = enonce;
-	this.options = options;
+var Question = function(id, titre, type, enonce, options, reponsesCorrectes) {
+    this.id = id;
+    this.titre = titre;
+    this.type = type;
+    this.enonce = enonce;
+    this.options = options;
     this.reponsesCorrectes = reponsesCorrectes;
-}
+};
 
 module.exports = Question;
