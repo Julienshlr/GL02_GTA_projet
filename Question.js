86b638e8b529c548ccc23f6812e714bcf9744fab
antoine <antoine.kuntz@utt.fr>
2024-12-02 15:07:26 +0100

    maj classe question

--- Question.js
+++ Question.js
@@ -1,10 +1,10 @@
 // Classe permettant la création d'une question
 
-var Question = function(id, titre, type, /*enonce,*/ options, reponsesCorrectes) {
+var Question = function(id, titre, type, enonce, options, reponsesCorrectes) {
     this.id = id;
     this.titre = titre;
     this.type = type;
-    //this.enonce = enonce;
+    this.enonce = enonce;
     this.options = options;
     this.reponsesCorrectes = reponsesCorrectes;
 };
